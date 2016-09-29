8a29e3a1a7e9e4f4fbc6ca96cc676c2ea0343c20
intliang <yintiliang@gmail.com>
2016-09-29 16:37:13 +0800

    add errorcount in RTMPSockBuf; and function RTMPSockBuf_Shutdown

--- librtmp/rtmp.h
+++ librtmp/rtmp.h
@@ -133,6 +133,7 @@ extern "C"
     int sb_timedout;
     void *sb_ssl;
     uint8_t sb_readable;
+    uint8_t sb_errorcount;
   } RTMPSockBuf;
 
   void RTMPPacket_Reset(RTMPPacket *p);
@@ -362,6 +363,7 @@ extern "C"
   int RTMPSockBuf_Fill(RTMPSockBuf *sb);
   int RTMPSockBuf_Send(RTMPSockBuf *sb, const char *buf, int len);
   int RTMPSockBuf_Close(RTMPSockBuf *sb);
+  int RTMPSockBuf_Shutdown(RTMPSockBuf *sb);
 
   int RTMP_SendCreateStream(RTMP *r);
   int RTMP_SendSeek(RTMP *r, int dTime);
